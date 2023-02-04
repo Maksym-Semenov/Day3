92e065eec41da6760749f4348fe60ef6e4b0dbda
DeathForRussia <ramirkiev@gmail.com>
2023-02-04 13:31:23 +0200

    Add project files.

---
+++ Day3_WPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Day3_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void plusButton_Click(object sender, RoutedEventArgs e)
+        {
+            int x = Convert.ToInt32(firstValue.Text);
+            int y = Convert.ToInt32(secondValue.Text);
+            int res = x + y;
+            result.Text = res.ToString();
+        }
+
+        private void minusButton_Click(object sender, RoutedEventArgs e)
+        {
+            int x = Convert.ToInt32(firstValue.Text);
+            int y = Convert.ToInt32(secondValue.Text);
+            int res = x - y;
+            result.Text = res.ToString();
+        }
+        private void firstValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void multipleButton_Click(object sender, RoutedEventArgs e)
+        {
+            int x = Convert.ToInt32(firstValue.Text);
+            int y = Convert.ToInt32(secondValue.Text);
+            int res = x * y;
+            result.Text = res.ToString();
+        }
+
+        private void divisionButton_Click(object sender, RoutedEventArgs e)
+        {
+            int x = Convert.ToInt32(firstValue.Text);
+            int y = Convert.ToInt32(secondValue.Text);
+            if (y == 0)
+            {
+                MessageBox.Show("На нуль ділити не можна!");
+            }
+            else
+            {
+                double res = x / y;
+                result.Text = res.ToString();
+            }
+            
+        }
+    }
+}
---
+++ HomeTask3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+string productName = "Олівець"; // назва продукту
+double productPrice = 5.75;     // ціна однієї позиції продукту
+double shippingPrice = 80;      // ціна доставки
+
+// виконайте наступні дії
+// 1 - покажіть користувачу назву та ціну товару
+// 2 - запитайте скільки одиниць товару потрібно замовити, значення не може бути менше ніж 1
+// 3 - запитайте чи потрібно виконувати доставку товару, якщо так ціна доставки shippingPrice інакше 0
+// 4 - порахуйте вартість замовлених товарів з урахуванням кількості і доставки, та виведіть результат
+
+Console.OutputEncoding = UTF8Encoding.UTF8;
+
+int value = 0;
+  
+Console.WriteLine("Назва товару: " + productName + " Ціна: " + productPrice);
+Console.Write("Введіть кількість товару для замовлення:  ");
+value = Convert.ToInt32(Console.ReadLine());
+
+while (value < 1 || value > 80)
+{
+    Console.WriteLine("Неправильна кількість товару");
+    Console.Write("Введіть кількість товару для замовлення:  ");
+    value = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("Чи потрібна вам доставка?");
+Console.WriteLine("Якщо ТАК, натисність + (плюс)");
+Console.WriteLine("Якщо НІ, натисність - (мінус)");
+string shipping = Console.ReadLine();
+
+while (shipping != "+" | shipping == "-")
+{
+    Console.WriteLine("Введіть правильний знак");
+    shipping = Console.ReadLine();
+}
+if (shipping == "+")
+{
+    double result1 = value * productPrice + shippingPrice;
+    Console.WriteLine("Вартість товарів з урахуванням доставки " + result1);
+}
+else
+{
+    double result2 = value * productPrice;
+    Console.WriteLine("Вартість товарів з урахуванням доставки " + result2);
+}
+
+Console.ReadLine();
